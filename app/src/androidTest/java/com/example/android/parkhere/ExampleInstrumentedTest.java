ad19879a34112482525e777bc9e399b9f39a4a41
yaxiangzou <32016646+yaxiangzou@users.noreply.github.com>
2017-10-30 22:00:43 -0400

    Revert "Create RegisterActivity.java"

--- app/src/androidTest/java/com/example/android/parkhere/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/android/parkhere/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.example.android.parkingapp;
+package com.example.android.parkhere;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.example.android.parkingapp", appContext.getPackageName());
+        assertEquals("com.example.android.parkhere", appContext.getPackageName());
     }
 }
